1b10d2e476cf05c1bb215c2a7f86b93fa6cc44f4
Jai Ivarsson <me@jaiivarsson.com>
2016-06-07 21:28:19 +1200

    fix bug with resource stuff on the wrong class

--- app/controllers/v1/people_controller.rb
+++ app/controllers/v1/people_controller.rb
@@ -1,8 +1,4 @@
 module V1
   class PeopleController < ApiController
-    attributes :name
-
-    has_many :writers
-    has_many :performers
   end
 end
--- app/resources/v1/person_resource.rb
+++ app/resources/v1/person_resource.rb
@@ -1,4 +1,8 @@
 module V1
   class PersonResource < JSONAPI::Resource
+    attributes :name
+
+    has_many :writers
+    has_many :performers
   end
 end
